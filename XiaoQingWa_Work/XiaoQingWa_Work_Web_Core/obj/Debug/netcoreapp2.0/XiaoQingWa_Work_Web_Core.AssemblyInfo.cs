d9a9ba68cc3ada17ca292e4dbb710b9e4825199f
zp <651649026@qq.com>
2017-12-08 18:54:22 +0800

    xiugai

--- XiaoQingWa_Work/XiaoQingWa_Work_Web_Core/obj/Debug/netcoreapp2.0/XiaoQingWa_Work_Web_Core.AssemblyInfo.cs
+++ XiaoQingWa_Work/XiaoQingWa_Work_Web_Core/obj/Debug/netcoreapp2.0/XiaoQingWa_Work_Web_Core.AssemblyInfo.cs
@@ -13,7 +13,6 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("XiaoQingWa_Work_Web_Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyDescriptionAttribute("Package Description")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("XiaoQingWa_Work_Web_Core")]

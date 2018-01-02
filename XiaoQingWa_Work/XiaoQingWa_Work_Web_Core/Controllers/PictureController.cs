8d8d6ca3837d5582d1f6ba617f180fa55f69f217
zp <651649026@qq.com>
2018-01-02 20:19:39 +0800

    异步方法

---
+++ XiaoQingWa_Work/XiaoQingWa_Work_Web_Core/Controllers/PictureController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using XiaoQingWa_Work_DAL;
+using XiaoQingWa_Work_IDAL;
+
+namespace XiaoQingWa_Work_Web_Core.Controllers
+{
+    public class PictureController : Controller
+    {
+        PictureInfoRepository cadal;
+        public PictureController(PictureInfoRepository cadal) { this.cadal = cadal; }
+
+       
+        public async Task<IActionResult> Index()
+        {
+            var pictureList =await cadal.GetListAsync();
+            return View(pictureList);
+        }
+    }
+}
--- XiaoQingWa_Work/XiaoQingWa_Work_Web_Core/Startup.cs
+++ XiaoQingWa_Work/XiaoQingWa_Work_Web_Core/Startup.cs
@@ -11,9 +11,14 @@ namespace XiaoQingWa_Work_Web_Core
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
@@ -21,11 +26,11 @@ namespace XiaoQingWa_Work_Web_Core
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            string connStr = Configuration.GetSection("ConnStr").Value;
+            string connStr = Configuration.GetSection("ConnectionString").Value;
 
             ////注入
-            //services.AddSingleton<XiaoQingWa_Work_DAL.UserInfoRepository>(new XiaoQingWa_Work_DAL.UserInfoRepository() { ConnStr = connStr });
-            //services.AddSingleton<DAL.OutlayDAL>(new DAL.OutlayDAL() { ConnStr = connStr });
+            //services.AddSingleton<XiaoQingWa_Work_DAL.PictureInfoRepository>(new XiaoQingWa_Work_DAL.PictureInfoRepository() { GetConnstr = connStr });
+          
             //services.AddSingleton<DAL.CategoryDAL>(new DAL.CategoryDAL() { ConnStr = connStr });
             //services.AddSingleton<DAL.IncomeDAL>(new DAL.IncomeDAL() { ConnStr = connStr });
 
